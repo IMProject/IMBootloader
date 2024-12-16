371322ad80063d360ddb9f4377f3845e5d627f4e
Igor Mišić <igy1000mb@gmail.com>
2024-12-16 20:39:15 +0100

    Add full UnitTest for board_info.c

--- Tests/test_board_info.c
+++ Tests/test_board_info.c
@@ -4,6 +4,7 @@
 #include "unity_fixture.h"
 
 #include "mock_hash_adapter.h"
+#include "mock_json.h"
 
 TEST_GROUP(BoardInfo);
 
@@ -19,10 +20,30 @@ TEST_GROUP_RUNNER(BoardInfo) {
 
 TEST(BoardInfo, BoardInfo_getDataJson) {
     {
-        HashAdapter_getBase64HashedBoardId_IgnoreAndReturn(true);
+        uint8_t b64_hashed_board_id_str[HASHED_BOARD_ID_SIZE_BASE64_STR] = {0x00};
+        HashAdapter_getBase64HashedBoardId_ExpectAnyArgsAndReturn(true);
+        HashAdapter_getBase64HashedBoardId_ReturnArrayThruPtr_b64_hashed_board_id_str(b64_hashed_board_id_str, HASHED_BOARD_ID_SIZE_BASE64_STR);
 
         uint8_t buffer[1000];
-        bool success = BoardInfo_getDataJson(buffer, 1000);
+        Json_startString_ExpectAndReturn((char*)buffer, sizeof(buffer), true);
+
+        const char key_board_id[] = "board_id";
+        Json_addData_ExpectAndReturn((char*)buffer, sizeof(buffer), key_board_id, (char*)b64_hashed_board_id_str, true);
+
+        const char key_manufacturer_id[] = "manufacturer_id";
+        Json_addData_ExpectAndReturn((char*)buffer, sizeof(buffer), key_manufacturer_id, MANUFACTURER_ID_BASE64_STR, true);
+
+        const char key_product_type[] = "product_type";
+        Json_addData_ExpectAndReturn((char*)buffer, sizeof(buffer), key_product_type, PRODUCT_TYPE, true);
+
+        Json_endString_ExpectAndReturn((char*)buffer, sizeof(buffer), true);
+
+
+        bool success = BoardInfo_getDataJson(buffer, sizeof(buffer));
         TEST_ASSERT_TRUE(success);
+
+        HashAdapter_getBase64HashedBoardId_ExpectAnyArgsAndReturn(false);
+        success = BoardInfo_getDataJson(buffer, sizeof(buffer));
+        TEST_ASSERT_FALSE(success);
     }
 }
